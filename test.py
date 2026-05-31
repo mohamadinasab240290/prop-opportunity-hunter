3f19f4f234c57369a08718b6b4e5c4cb5f3eda6e
mohamadinasab240290 <mohamad240290@gmail.com>
2026-06-01 02:37:09 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
-print("Prop Opportunity Hunter is running")
+import os
+import requests
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+message = """
+🚀 Prop Opportunity Hunter
+✅ سیستم آنلاین است
+✅ GitHub Actions فعال است
+✅ ربات تلگرام متصل است
+تاریخ گزارش: 2026-06-01
+"""
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
+print("Message sent")
