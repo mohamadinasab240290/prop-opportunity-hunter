5e82f002f708cf2319380df08c4e1393ab14f5e1
mohamadinasab240290 <mohamad240290@gmail.com>
2026-06-01 03:51:42 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,7 @@
 import requests
-import os
-BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-# BTCUSDT
-btc_url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
-btc_price = requests.get(btc_url).json()["price"]
-# Message
-message = f"""
-👁 Third Eye Business
-📊 Market Dashboard
-₿ BTCUSDT
-{btc_price}
-🚀 Bot is running successfully
-"""
-# Send Telegram
-telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-requests.post(
-    telegram_url,
-    data={
-        "chat_id": CHAT_ID,
-        "text": message
-    }
-)
 
-print("Message sent")
+btc_url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+
+response = requests.get(btc_url)
+
+print(response.text)
