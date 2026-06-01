e66cff4fafaa10042557b15c32a7dc94080832a2
mohamadinasab240290 <mohamad240290@gmail.com>
2026-06-01 04:15:07 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,23 @@
+import yfinance as yf
 import requests
-
-btc_url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
-
-response = requests.get(btc_url)
-
-print("RESULT:")
-print(response.text)
+import os
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+btc = yf.Ticker("BTC-USD")
+btc_price = btc.history(period="1d").tail(1)["Close"].iloc[0]
+message = f"""
+👁 Third Eye Business
+📊 Market Dashboard
+₿ BTCUSD
+{btc_price:.2f}
+✅ Data received successfully
+"""
+telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+requests.post(
+    telegram_url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+)
+print("Telegram message sent")
